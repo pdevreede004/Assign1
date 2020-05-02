b16a7e42620c7a7f71a7b7455b5c67c77ed6f592
pdevreede004 <pdevreede@unomaha.edu>
2020-05-02 15:10:48 -0500

    Changed the HTML for the site.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e-oh1$zs3e1mw-k%)jpr%e7^n%eo+gh*rodzm3^_t2js^1*rqa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'pdevreede.pythonanywhere.com']
 
